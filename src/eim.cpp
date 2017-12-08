0321c3d5ea1b2095a6e3ae194c321677f49bea21
Weng Xuetian <wengxt@gmail.com>
2017-12-08 09:19:24 -0800

    Initial support for fcitx 5

---
+++ src/eim.cpp
@@ -0,0 +1,417 @@
+/***************************************************************************
+ *   Copyright (C) 2012~2012 by Tai-Lin Chu                                *
+ *   tailinchu@gmail.com                                                   *
+ *   Copyright (C) 2012~2017 by Weng Xuetian                               *
+ *   wengxt@gmail.com                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
+#include "eim.h"
+#include <fcitx-utils/utf8.h>
+#include <fcitx/inputcontext.h>
+#include <fcitx/inputpanel.h>
+#include <fcitx/text.h>
+
+FCITX_DEFINE_LOG_CATEGORY(chewing_log, "chewing");
+#define CHEWING_DEBUG() FCITX_LOGC(chewing_log, Debug)
+
+namespace fcitx {
+
+namespace {
+
+constexpr int CHEWING_MAX_LEN = 18;
+
+std::string builtin_keymaps[] = {
+    "KB_DEFAULT",     "KB_HSU",          "KB_IBM",    "KB_GIN_YEIH",
+    "KB_ET",          "KB_ET26",         "KB_DVORAK", "KB_DVORAK_HSU",
+    "KB_DACHEN_CP26", "KB_HANYU_PINYIN", "KB_CARPALX"};
+
+static const char *builtin_selectkeys[] = {
+    "1234567890", "asdfghjkl;", "asdfzxcv89", "asdfjkl789",
+    "aoeuhtn789", "1234qweras", "dstnaeo789",
+};
+
+class ChewingCandidateWord : public CandidateWord {
+public:
+    ChewingCandidateWord(ChewingEngine *engine, std::string str, int index)
+        : CandidateWord(Text(str)), engine_(engine), index_(index) {}
+
+    void select(InputContext *inputContext) const override {
+        auto pageSize = engine_->instance()->globalConfig().defaultPageSize();
+        auto ctx = engine_->context();
+        int page = index_ / pageSize + chewing_cand_CurrentPage(ctx);
+        int off = index_ % pageSize;
+        if (page < 0 || page >= chewing_cand_TotalPage(ctx))
+            return;
+        int lastPage = chewing_cand_CurrentPage(ctx);
+        while (page != chewing_cand_CurrentPage(ctx)) {
+            if (page < chewing_cand_CurrentPage(ctx)) {
+                chewing_handle_Left(ctx);
+            }
+            if (page > chewing_cand_CurrentPage(ctx)) {
+                chewing_handle_Right(ctx);
+            }
+            /* though useless, but take care if there is a bug cause freeze */
+            if (lastPage == chewing_cand_CurrentPage(ctx)) {
+                break;
+            }
+            lastPage = chewing_cand_CurrentPage(ctx);
+        }
+        chewing_handle_Default(ctx, builtin_selectkeys[static_cast<int>(
+                                        *engine_->config().SelectionKey)][off]);
+
+        if (chewing_keystroke_CheckAbsorb(ctx)) {
+            engine_->updateUI(inputContext);
+        } else if (chewing_keystroke_CheckIgnore(ctx)) {
+            return;
+        } else if (chewing_commit_Check(ctx)) {
+            char *str = chewing_commit_String(ctx);
+            inputContext->commitString(str);
+            chewing_free(str);
+        } else {
+            engine_->updateUI(inputContext);
+        }
+    }
+
+private:
+    ChewingEngine *engine_;
+    int index_;
+};
+
+class ChewingCandidateList : public CandidateList,
+                             public PageableCandidateList {
+public:
+    ChewingCandidateList(ChewingEngine *engine, InputContext *ic)
+        : engine_(engine), ic_(ic) {
+        auto ctx = engine_->context();
+        setPageable(this);
+        int index = 0;
+        // get candidate word
+        int pageSize = chewing_get_candPerPage(ctx);
+        chewing_cand_Enumerate(ctx);
+        while (chewing_cand_hasNext(ctx) && index < pageSize) {
+            char *str = chewing_cand_String(ctx);
+            candidateWords_.emplace_back(
+                std::make_shared<ChewingCandidateWord>(engine_, str, index));
+            if (index < 10) {
+                const char label[] = {
+                    builtin_selectkeys[static_cast<int>(
+                        *engine_->config().SelectionKey)][index],
+                    '\0'};
+                labels_.emplace_back(label);
+            } else {
+                labels_.emplace_back();
+            }
+            chewing_free(str);
+            index++;
+        }
+
+        hasPrev_ = chewing_cand_CurrentPage(ctx) > 0;
+        hasNext_ =
+            chewing_cand_CurrentPage(ctx) + 1 < chewing_cand_TotalPage(ctx);
+    }
+
+    const Text &label(int idx) const override {
+        if (idx < 0 || idx >= size()) {
+            throw std::invalid_argument("Invalid index");
+        }
+        return labels_[idx];
+    }
+    std::shared_ptr<const CandidateWord> candidate(int idx) const override {
+        if (idx < 0 || idx >= size()) {
+            throw std::invalid_argument("Invalid index");
+        }
+        return candidateWords_[idx];
+    }
+
+    int size() const override { return candidateWords_.size(); }
+    int cursorIndex() const override { return -1; }
+    CandidateLayoutHint layoutHint() const override {
+        return CandidateLayoutHint::NotSet;
+    }
+
+    // Need for paging
+    bool hasPrev() const override { return hasPrev_; }
+    bool hasNext() const override { return hasNext_; }
+    void prev() override { paging(true); }
+    void next() override { paging(false); }
+
+    bool usedNextBefore() const override { return true; }
+
+private:
+    void paging(bool prev) {
+        if (candidateWords_.empty()) {
+            return;
+        }
+
+        auto ctx = engine_->context();
+        if (prev) {
+            chewing_handle_Left(ctx);
+        } else {
+            chewing_handle_Right(ctx);
+        }
+
+        if (chewing_keystroke_CheckAbsorb(ctx)) {
+            engine_->updateUI(ic_);
+        }
+    }
+
+    bool hasPrev_ = false;
+    bool hasNext_ = false;
+    ChewingEngine *engine_;
+    InputContext *ic_;
+    std::vector<std::shared_ptr<CandidateWord>> candidateWords_;
+    std::vector<Text> labels_;
+};
+
+} // namespace
+
+ChewingEngine::ChewingEngine(Instance *instance)
+    : instance_(instance), context_(chewing_new(), &chewing_delete) {
+    chewing_set_maxChiSymbolLen(context_.get(), CHEWING_MAX_LEN);
+    chewing_set_candPerPage(context_.get(),
+                            instance_->globalConfig().defaultPageSize());
+    reloadConfig();
+}
+
+ChewingEngine::~ChewingEngine() = default;
+
+void ChewingEngine::reloadConfig() {
+    readAsIni(config_, "conf/chewing.conf");
+    populateConfig();
+}
+
+void ChewingEngine::populateConfig() {
+    ChewingContext *ctx = context_.get();
+    chewing_set_addPhraseDirection(ctx, *config_.AddPhraseForward ? 0 : 1);
+    chewing_set_phraseChoiceRearward(ctx, *config_.ChoiceBackward ? 1 : 0);
+    chewing_set_autoShiftCur(ctx, *config_.AutoShiftCursor ? 1 : 0);
+    chewing_set_spaceAsSelection(ctx, *config_.SpaceAsSelection ? 1 : 0);
+    chewing_set_escCleanAllBuf(ctx, 1);
+}
+
+void ChewingEngine::reset(const InputMethodEntry &, InputContextEvent &) {
+    ChewingContext *ctx = context_.get();
+    chewing_Reset(ctx);
+
+    chewing_set_KBType(
+        ctx, chewing_KBStr2Num(
+                 builtin_keymaps[static_cast<int>(*config_.Layout)].data()));
+
+    chewing_set_ChiEngMode(ctx, CHINESE_MODE);
+#if 0
+    FcitxUIStatus* puncStatus = FcitxUIGetStatusByName(chewing->owner, "punc");
+    if (puncStatus) {
+        if (puncStatus->getCurrentStatus(puncStatus->arg))
+            chewing_set_ShapeMode(ctx, FULLSHAPE_MODE);
+        else
+            chewing_set_ShapeMode(ctx, HALFSHAPE_MODE);
+    }
+#endif
+}
+
+void ChewingEngine::save() {}
+
+void ChewingEngine::activate(const InputMethodEntry &, InputContextEvent &) {}
+
+void ChewingEngine::deactivate(const InputMethodEntry &entry,
+                               InputContextEvent &event) {
+    auto ctx = context_.get();
+    if (event.type() == EventType::InputContextFocusOut ||
+        event.type() == EventType::InputContextSwitchInputMethod) {
+        chewing_handle_Enter(ctx);
+        if (event.type() == EventType::InputContextSwitchInputMethod) {
+            if (chewing_commit_Check(ctx)) {
+                char *str = chewing_commit_String(ctx);
+                event.inputContext()->commitString(str);
+                chewing_free(str);
+            } else {
+                char *buf_str = chewing_buffer_String(ctx);
+                if (strlen(buf_str) != 0) {
+                    event.inputContext()->commitString(buf_str);
+                }
+                chewing_free(buf_str);
+            }
+        }
+    }
+    reset(entry, event);
+}
+
+void ChewingEngine::keyEvent(const InputMethodEntry &entry,
+                             KeyEvent &keyEvent) {
+    auto ctx = context_.get();
+    if (keyEvent.isRelease()) {
+        return;
+    }
+
+    CHEWING_DEBUG() << "KeyEvent: " << keyEvent.key().toString();
+
+    if (keyEvent.key().check(FcitxKey_space)) {
+        chewing_handle_Space(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Tab)) {
+        chewing_handle_Tab(ctx);
+    } else if (keyEvent.key().isSimple()) {
+        int scan_code = keyEvent.key().sym() & 0xff;
+        chewing_handle_Default(ctx, scan_code);
+    } else if (keyEvent.key().check(FcitxKey_BackSpace)) {
+        const char *zuin_str = chewing_bopomofo_String_static(ctx);
+        if (chewing_buffer_Len(ctx) == 0 && !zuin_str[0]) {
+            return;
+        }
+        chewing_handle_Backspace(ctx);
+        if (chewing_buffer_Len(ctx) == 0 && !zuin_str[0]) {
+            return reset(entry, keyEvent);
+        }
+    } else if (keyEvent.key().check(FcitxKey_Escape)) {
+        chewing_handle_Esc(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Delete)) {
+        const char *zuin_str = chewing_bopomofo_String_static(ctx);
+        if (chewing_buffer_Len(ctx) == 0 && !zuin_str[0]) {
+            return;
+        }
+        chewing_handle_Del(ctx);
+        if (chewing_buffer_Len(ctx) == 0 && !zuin_str[0]) {
+            keyEvent.filterAndAccept();
+            return reset(entry, keyEvent);
+        }
+    } else if (keyEvent.key().check(FcitxKey_Up)) {
+        chewing_handle_Up(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Down)) {
+        chewing_handle_Down(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Page_Down)) {
+        chewing_handle_PageDown(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Page_Up)) {
+        chewing_handle_PageUp(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Right)) {
+        chewing_handle_Right(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Left)) {
+        chewing_handle_Left(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Home)) {
+        chewing_handle_Home(ctx);
+    } else if (keyEvent.key().check(FcitxKey_End)) {
+        chewing_handle_End(ctx);
+    } else if (keyEvent.key().check(FcitxKey_space, KeyState::Shift)) {
+        chewing_handle_ShiftSpace(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Left, KeyState::Shift)) {
+        chewing_handle_ShiftLeft(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Right, KeyState::Shift)) {
+        chewing_handle_ShiftRight(ctx);
+    } else if (keyEvent.key().check(FcitxKey_Return)) {
+        chewing_handle_Enter(ctx);
+    } else if (keyEvent.key().states() == KeyState::Ctrl &&
+               Key(keyEvent.key().sym()).isDigit()) {
+        chewing_handle_CtrlNum(ctx, keyEvent.key().sym());
+    } else {
+        // to do: more chewing_handle
+        return;
+    }
+
+    if (chewing_keystroke_CheckAbsorb(ctx)) {
+        keyEvent.filterAndAccept();
+        return updateUI(keyEvent.inputContext());
+    } else if (chewing_keystroke_CheckIgnore(ctx)) {
+        return;
+    } else if (chewing_commit_Check(ctx)) {
+        keyEvent.filterAndAccept();
+        char *str = chewing_commit_String(ctx);
+        keyEvent.inputContext()->commitString(str);
+        chewing_free(str);
+        return updateUI(keyEvent.inputContext());
+    } else {
+        keyEvent.filterAndAccept();
+        return updateUI(keyEvent.inputContext());
+    }
+}
+
+void ChewingEngine::filterKey(const InputMethodEntry &, KeyEvent &keyEvent) {
+    auto ic = keyEvent.inputContext();
+    if (ic->inputPanel().candidateList() &&
+        (keyEvent.key().isSimple() || keyEvent.key().isCursorMove() ||
+         keyEvent.key().check(FcitxKey_space, KeyState::Shift) ||
+         keyEvent.key().check(FcitxKey_Tab) ||
+         keyEvent.key().check(FcitxKey_Return, KeyState::Shift)))
+        return keyEvent.filterAndAccept();
+}
+
+void ChewingEngine::updateUI(InputContext *ic) {
+    CHEWING_DEBUG() << "updateUI";
+    ChewingContext *ctx = context_.get();
+
+    int selkey[10];
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        selkey[i] =
+            builtin_selectkeys[static_cast<int>(*config_.SelectionKey)][i];
+    }
+
+    chewing_set_selKey(ctx, selkey, 10);
+    chewing_set_candPerPage(ctx, instance_->globalConfig().defaultPageSize());
+    populateConfig();
+
+    // clean up window asap
+    ic->inputPanel().reset();
+    ic->updateUserInterface(UserInterfaceComponent::InputPanel);
+
+    std::unique_ptr<char, decltype(&chewing_free)> buf_str(
+        chewing_buffer_String(ctx), &chewing_free);
+    const char *zuin_str = chewing_bopomofo_String_static(ctx);
+
+    std::string text = buf_str.get();
+    std::string zuin = zuin_str;
+    CHEWING_DEBUG() << "Text: " << text << " Zuin: " << zuin;
+    /* if not check done, so there is candidate word */
+    if (!chewing_cand_CheckDone(ctx)) {
+        ic->inputPanel().setCandidateList(
+            std::make_unique<ChewingCandidateList>(this, ic));
+        if (!ic->inputPanel().candidateList()->size()) {
+            ic->inputPanel().setCandidateList(nullptr);
+        }
+    }
+
+    /* there is nothing */
+    if (zuin.empty() && text.empty() && !ic->inputPanel().candidateList()) {
+        ic->updatePreedit();
+        return;
+    }
+
+    auto len = utf8::lengthValidated(text);
+    if (len == utf8::INVALID_LENGTH) {
+        return;
+    }
+    Text preedit;
+
+    int cur = chewing_cursor_Current(ctx);
+    int rcur = text.size();
+    if (cur >= 0 && static_cast<size_t>(cur) < len) {
+        rcur = utf8::ncharByteLength(text.begin(), cur);
+        preedit.setCursor(rcur);
+    }
+
+    // insert zuin in the middle
+    preedit.append(text.substr(0, rcur), TextFormatFlag::None);
+    preedit.append(zuin,
+                   {TextFormatFlag::HighLight, TextFormatFlag::DontCommit});
+    preedit.append(text.substr(rcur), TextFormatFlag::None);
+
+    ic->inputPanel().setPreedit(preedit);
+    ic->inputPanel().setClientPreedit(preedit);
+    ic->updatePreedit();
+}
+
+} // namespace fcitx
+
+FCITX_ADDON_FACTORY(fcitx::ChewingEngineFactory);
--- src/eim.h
+++ src/eim.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2010~2010 by CSSlayer                                   *
+ *   Copyright (C) 2010~2017 by CSSlayer                                   *
  *   wengxt@gmail.com                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -21,26 +21,132 @@
 #ifndef EIM_H
 #define EIM_H
 
-#include <fcitx/ime.h>
-#include <fcitx/instance.h>
-#include <fcitx/candidate.h>
 #include <chewing.h>
+#include <fcitx-config/configuration.h>
+#include <fcitx-config/enum.h>
+#include <fcitx-config/iniparser.h>
+#include <fcitx-utils/i18n.h>
+#include <fcitx/addonfactory.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputmethodengine.h>
+#include <fcitx/instance.h>
+#include <type_traits>
 
-#ifdef __cplusplus
-#define __EXPORT_API extern "C"
-#else
-#define __EXPORT_API
-#endif
+namespace fcitx {
 
-#define _(x) dgettext("fcitx-chewing", x)
+enum class ChewingSelectionKey {
+    CSK_Digit,
+    CSK_asdfghjkl,
+    CSK_asdfzxcv89,
+    CSK_asdfjkl789,
+    CSK_aoeuhtn789,
+    CSK_1234qweras,
+    CSK_dstnaeo789
+};
+
+FCITX_CONFIG_ENUM_NAME(ChewingSelectionKey, "1234567890", "asdfghjkl;",
+                       "asdfzxcv89", "asdfjkl789", "aoeuhtn789", "1234qweras",
+                       "dstnaeo789");
+FCITX_CONFIG_ENUM_I18N_ANNOTATION(ChewingSelectionKey, N_("1234567890"),
+                                  N_("asdfghjkl;"), N_("asdfzxcv89"),
+                                  N_("asdfjkl789"), N_("aoeuhtn789"),
+                                  N_("1234qweras"), N_("dstnaeo789"));
+
+enum class ChewingLayout {
+    Default,
+    Hsu,
+    IBM,
+    GinYieh,
+    ETen,
+    ETen26,
+    Dvorak,
+    DvorakHsu,
+    DACHEN_CP26,
+    HanYuPinYin,
+    Carpalx
+};
+FCITX_CONFIG_ENUM_NAME(ChewingLayout, "Default Keyboard", "Hsu's Keyboard",
+                       "IBM Keyboard", "Gin-Yieh Keyboard", "ETen Keyboard",
+                       "ETen26 Keyboard", "Dvorak Keyboard",
+                       "Dvorak Keyboard with Hsu's support",
+                       "DACHEN_CP26 Keyboard", "Han-Yu PinYin Keyboard",
+                       "Carpalx Keyboard");
+FCITX_CONFIG_ENUM_I18N_ANNOTATION(ChewingLayout, N_("Default Keyboard"),
+                                  N_("Hsu's Keyboard"), N_("IBM Keyboard"),
+                                  N_("Gin-Yieh Keyboard"), N_("ETen Keyboard"),
+                                  N_("ETen26 Keyboard"), N_("Dvorak Keyboard"),
+                                  N_("Dvorak Keyboard with Hsu's support"),
+                                  N_("DACHEN_CP26 Keyboard"),
+                                  N_("Han-Yu PinYin Keyboard"),
+                                  N_("Carpalx Keyboard"));
+
+FCITX_CONFIGURATION(
+    ChewingConfig,
+    OptionWithAnnotation<ChewingSelectionKey, ChewingSelectionKeyI18NAnnotation>
+        SelectionKey{this, "SelectionKey", _("Selection Key"),
+                     ChewingSelectionKey::CSK_Digit};
+    Option<bool> AddPhraseForward{this, "AddPhraseForward",
+                                  _("Add Phrase Forward"), true};
+    Option<bool> ChoiceBackward{this, "ChoiceBackward",
+                                _("Backward phrase choice"), true};
+    Option<bool> AutoShiftCursor{this, "AutoShiftCursor",
+                                 _("Automatically shift cursor"), false};
+    Option<bool> SpaceAsSelection{this, "SpaceAsSelection",
+                                  _("Space as selection key"), true};
+    OptionWithAnnotation<ChewingLayout, ChewingLayoutI18NAnnotation> Layout{
+        this, "Layout", _("Keyboard Layout"), ChewingLayout::Default};);
+
+class ChewingEngine final : public InputMethodEngine {
+public:
+    ChewingEngine(Instance *instance);
+    ~ChewingEngine();
+    Instance *instance() { return instance_; }
+    const ChewingConfig &config() { return config_; }
+    void activate(const InputMethodEntry &entry,
+                  InputContextEvent &event) override;
+    void deactivate(const InputMethodEntry &entry,
+                    InputContextEvent &event) override;
+    void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) override;
+    void filterKey(const InputMethodEntry &, KeyEvent &) override;
+    void reloadConfig() override;
+    void reset(const InputMethodEntry &entry,
+               InputContextEvent &event) override;
+    void save() override;
+
+    const Configuration *getConfig() const override { return &config_; }
+    void setConfig(const RawConfig &config) override {
+        config_.load(config, true);
+        safeSaveAsIni(config_, "conf/chewing.conf");
+    }
+
+    void updateUI(InputContext *ic);
+
+    ChewingContext *context() { return context_.get(); }
+
+private:
+    void populateConfig();
+    Instance *instance_;
+    ChewingConfig config_;
+    std::unique_ptr<ChewingContext, decltype(&chewing_delete)> context_;
+};
+
+class ChewingEngineFactory : public AddonFactory {
+public:
+    AddonInstance *create(AddonManager *manager) override {
+        return new ChewingEngine(manager->instance());
+    }
+};
+}
+
+#if 0
 #define CHEWING_MAX_LEN 16
 
-__EXPORT_API void* FcitxChewingCreate(FcitxInstance* instance);
-__EXPORT_API void FcitxChewingDestroy(void* arg);
-__EXPORT_API INPUT_RETURN_VALUE FcitxChewingDoInput(void* arg, FcitxKeySym sym, unsigned int state);
-__EXPORT_API INPUT_RETURN_VALUE FcitxChewingGetCandWords(void* arg);
-__EXPORT_API boolean FcitxChewingInit(void*);
-__EXPORT_API void FcitxChewingReset(void* arg);
+void* FcitxChewingCreate(FcitxInstance* instance);
+void FcitxChewingDestroy(void* arg);
+INPUT_RETURN_VALUE FcitxChewingDoInput(void* arg, FcitxKeySym sym, unsigned int state);
+INPUT_RETURN_VALUE FcitxChewingGetCandWords(void* arg);
+boolean FcitxChewingInit(void*);
+void FcitxChewingReset(void* arg);
 
 
 typedef struct _FcitxChewingConfig {
@@ -60,5 +166,6 @@ typedef struct _FcitxChewing {
 } FcitxChewing;
 
 CONFIG_BINDING_DECLARE(FcitxChewingConfig);
+#endif
 
 #endif
