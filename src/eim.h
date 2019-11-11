5cae7f0a64c57138768b172432e66402441ad6af
Weng Xuetian <wengxt@gmail.com>
2019-11-10 23:56:33 -0800

    replace clang-format.sh with git hook.

--- src/eim.h
+++ src/eim.h
@@ -33,13 +33,13 @@
 namespace fcitx {
 
 enum class ChewingSelectionKey {
-    CSK_Digit,
-    CSK_asdfghjkl,
-    CSK_asdfzxcv89,
-    CSK_asdfjkl789,
-    CSK_aoeuhtn789,
-    CSK_1234qweras,
-    CSK_dstnaeo789
+  CSK_Digit,
+  CSK_asdfghjkl,
+  CSK_asdfzxcv89,
+  CSK_asdfjkl789,
+  CSK_aoeuhtn789,
+  CSK_1234qweras,
+  CSK_dstnaeo789
 };
 
 FCITX_CONFIG_ENUM_NAME(ChewingSelectionKey, "1234567890", "asdfghjkl;",
@@ -51,17 +51,17 @@ FCITX_CONFIG_ENUM_I18N_ANNOTATION(ChewingSelectionKey, N_("1234567890"),
                                   N_("1234qweras"), N_("dstnaeo789"));
 
 enum class ChewingLayout {
-    Default,
-    Hsu,
-    IBM,
-    GinYieh,
-    ETen,
-    ETen26,
-    Dvorak,
-    DvorakHsu,
-    DACHEN_CP26,
-    HanYuPinYin,
-    Carpalx
+  Default,
+  Hsu,
+  IBM,
+  GinYieh,
+  ETen,
+  ETen26,
+  Dvorak,
+  DvorakHsu,
+  DACHEN_CP26,
+  HanYuPinYin,
+  Carpalx
 };
 FCITX_CONFIG_ENUM_NAME(ChewingLayout, "Default Keyboard", "Hsu's Keyboard",
                        "IBM Keyboard", "Gin-Yieh Keyboard", "ETen Keyboard",
@@ -96,44 +96,43 @@ FCITX_CONFIGURATION(
 
 class ChewingEngine final : public InputMethodEngine {
 public:
-    ChewingEngine(Instance *instance);
-    ~ChewingEngine();
-    Instance *instance() { return instance_; }
-    const ChewingConfig &config() { return config_; }
-    void activate(const InputMethodEntry &entry,
+  ChewingEngine(Instance *instance);
+  ~ChewingEngine();
+  Instance *instance() { return instance_; }
+  const ChewingConfig &config() { return config_; }
+  void activate(const InputMethodEntry &entry,
+                InputContextEvent &event) override;
+  void deactivate(const InputMethodEntry &entry,
                   InputContextEvent &event) override;
-    void deactivate(const InputMethodEntry &entry,
-                    InputContextEvent &event) override;
-    void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) override;
-    void filterKey(const InputMethodEntry &, KeyEvent &) override;
-    void reloadConfig() override;
-    void reset(const InputMethodEntry &entry,
-               InputContextEvent &event) override;
-    void save() override;
+  void keyEvent(const InputMethodEntry &entry, KeyEvent &keyEvent) override;
+  void filterKey(const InputMethodEntry &, KeyEvent &) override;
+  void reloadConfig() override;
+  void reset(const InputMethodEntry &entry, InputContextEvent &event) override;
+  void save() override;
 
-    const Configuration *getConfig() const override { return &config_; }
-    void setConfig(const RawConfig &config) override {
-        config_.load(config, true);
-        safeSaveAsIni(config_, "conf/chewing.conf");
-    }
+  const Configuration *getConfig() const override { return &config_; }
+  void setConfig(const RawConfig &config) override {
+    config_.load(config, true);
+    safeSaveAsIni(config_, "conf/chewing.conf");
+  }
 
-    void updateUI(InputContext *ic);
+  void updateUI(InputContext *ic);
 
-    ChewingContext *context() { return context_.get(); }
+  ChewingContext *context() { return context_.get(); }
 
 private:
-    void populateConfig();
-    Instance *instance_;
-    ChewingConfig config_;
-    std::unique_ptr<ChewingContext, decltype(&chewing_delete)> context_;
+  void populateConfig();
+  Instance *instance_;
+  ChewingConfig config_;
+  std::unique_ptr<ChewingContext, decltype(&chewing_delete)> context_;
 };
 
 class ChewingEngineFactory : public AddonFactory {
 public:
-    AddonInstance *create(AddonManager *manager) override {
-        return new ChewingEngine(manager->instance());
-    }
+  AddonInstance *create(AddonManager *manager) override {
+    return new ChewingEngine(manager->instance());
+  }
 };
-}
+} // namespace fcitx
 
 #endif // _FCITX5_CHEWING_EIM_H_
